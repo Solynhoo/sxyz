f5e92e8f7294e45f61763cd1b2c88da07748092f
Solyn <solyn@solyn.xyz>
2023-11-28 21:58:20 +0000

    » Added 404 page

---
+++ layouts/404.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup></script>
+
+<template>
+  <NuxtLayout name="404">
+    <div>
+      <div class="text-4xl">You've Arrived Here on Error, boss</div>
+      <button class="font-bold button" @click="goBack">Back</button>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.button {
+  padding: 4px 6px;
+  margin: 10px 0px;
+  background: black;
+  color: white;
+}
+</style>
+</template>
+
+<style scoped></style>
