b6691bd239f2b6faea5b6ee0f9b4af8d98cdc4ba
Solyn <solyn@outlook.pt>
2023-12-04 17:13:47 +0000

    » Update

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.png',
   socials: {
-    twitter: 'OnlySolyn',
+    twitter: 'Solynhoo',
     github: 'Solynhoo',
   }
 })
